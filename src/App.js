1f3727f4d2c60a85343dcdd40832e09ff390f6c1
AKP <akphbk619@gmail.com>
2022-12-04 23:49:36 +0530

    My Draft AWS 1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Changed for AWS 
         </a>
       </header>
     </div>
